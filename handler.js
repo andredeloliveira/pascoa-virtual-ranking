436502101996cb30ec086aa74a8a8a4b7d55e0f0
Andre de Oliveira <oliveira.andrede@gmail.com>
2017-01-23 18:44:35 -0200

    Add addToRanking ms function

--- handler.js
+++ handler.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+
 module.exports.hello = (event, context, callback) => {
   const response = {
     statusCode: 200,
@@ -14,3 +17,25 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.addToRanking = (event, context, callback) => {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+  var Item = {
+    id: uuid.v4(),
+    user: event.email,
+    time: event.time,
+  };
+
+  docClient.put({ TableName: 'pascoa-ranking', Item: Item}, (error) => {
+    if (error) {
+      callback(error)
+    }
+    callback(null, {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: "Information saved successfully with " + JSON.stringify(event)
+    })
+  })
+}
